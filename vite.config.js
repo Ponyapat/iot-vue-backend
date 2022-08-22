c53a153e62c6036b2012f2fd8fb43efa82881e74
neng <tanagorn@gmail.com>
2022-08-22 10:07:18 +0700

    add proxy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { createProxy } from './build/proxy'
+
+const VITE_PORT = 3001
+const VITE_PROXY = [
+  ['/api-sso', 'https://sso.farmthailand.app'],
+  ['/api-main', 'https://stg-api.checkwolrd.com/api']
+]
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      https: false,
+      port: VITE_PORT,
+      proxy: mode === 'development' ? createProxy(VITE_PROXY) : null
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
     }
   }
 })
