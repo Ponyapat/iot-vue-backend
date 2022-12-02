c913471336a3f6ed9a9b858f05b0ec022b3d6ee7
BoatPawarisorn <boatzeed57270@gmail.com>
2022-12-02 12:54:02 +0700

    Boat update paging all page

--- src/components/FruitsTable.vue
+++ src/components/FruitsTable.vue
@@ -117,13 +117,64 @@ const pagesList = computed(() => {
 });
 
 const pageNext = (page) => {
-  console.log('page in Next page == ',page);
   currentPage.value = page;
   ApiMain.get("/breed?order=DESC&page=" + (page + 1) + "&take=" + perPage.value).then((res) => {
       states.fruits = res.data.data;
     });
 };
+const nextPage = () => {
+  let x = currentPage.value++;
+  let pagelist = pagesList.value;
+  if (x < pagelist.slice(-1)[0]) {
+    ApiMain.get("/breed?order=DESC&page=" + (x + 1) + "&take=" + perPage.value)
+      .then((data) => {
+        states.fruits = data.data.data;
+      });
+  }
+  else {
+    currentPage.value = pagelist.slice(-1)[0];
+  }
+};
+const previousPage = () => {
+  let x = currentPage.value--;
+  if (x > 0) {
+    ApiMain.get("/breed?order=DESC&page=" + (x + 1) + "&take=" + perPage.value).then((data) => {
+      states.fruits = data.data.data;
+    });
+  }
+  else {
+    currentPage.value = 0;
+  }
+};
 
+const lastPage = () => {
+  let pagelist = pagesList.value;
+  currentPage.value = pagelist.slice(-1)[0];
+  ApiMain.get("/breed?order=DESC&page=" + (currentPage.value+1)  + "&take=" + perPage.value).then((data) => {
+      states.fruits = data.data.data;
+    });
+};
+const firstPage = () => {
+  let pagelist = pagesList.value;
+  console.log(pagelist[0]);
+  currentPage.value = pagelist[0];
+
+  ApiMain.get("/breed?order=DESC&page=" + (currentPage.value+1)  + "&take=" + perPage.value).then((data) => {
+      states.fruits = data.data.data;
+    });
+};
+const pages = computed(() => {
+    let numShown = 10; // This sets the number of page tabs
+    let pagelist = pagesList.value ;
+    numShown = Math.min(numShown, pagelist.length);
+
+    let first = (currentPage.value) - Math.floor(numShown / 2);
+
+    first = Math.max(first, 1);
+
+    first = Math.min(first, pagelist.length - numShown + 1);
+    return [...Array(numShown)].map((k,i) => i + first);
+});
 </script>
 
 <template>
@@ -190,7 +241,54 @@ const pageNext = (page) => {
       </tbody>
     </table>
   </div>
-  <div :class="lightBorderStyle" class="p-3 lg:px-6 border-t dark:border-gray-800">
+  <nav :class="lightBorderStyle" aria-label="Page navigation example" class="py-2 mx-3 lg:mx-6 border-t border-b ">
+    <ul class="inline-flex items-center -space-x-px">
+      <li @click="firstPage">
+        <a href="#"
+          class="flex flex-row  px-2 py-2 ml-0 leading-tight text-white bg-gray-600 border border-gray-300  hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white rounded-l-lg">
+          <i class="fa-solid fa-less-than  text-sm"></i>
+          <i class="fa-solid fa-less-than  text-sm"></i>
+        </a>
+      </li>
+      <li @click="previousPage">
+        <a href="#"
+          class="block px-2 py-2 ml-0 leading-tight text-white bg-gray-600 border border-gray-300  hover:bg-gray-500 hover:text-gray-700 dark:bg-gray-800 dark:border-white dark:hover:bg-gray-700 dark:hover:text-white">
+          <span class="sr-only">Previous</span>
+          <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" class="fill-white dark:fill-white"
+              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+              clip-rule="evenodd"></path>
+          </svg>
+        </a>
+      </li>
+      <li v-for="item in pages" :key="item" :class="{ active: item == currentPage+1}"
+        @click="pageNext(item-1)" class=" px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-400 dark:border-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white">
+        <a href="#">{{item}}</a>
+      </li>
+      <li @click="nextPage">
+        <a href="#"
+        class="block px-2 py-2 ml-0 leading-tight text-white bg-gray-600 border border-gray-300  hover:bg-gray-500 hover:text-gray-700 dark:bg-gray-800 dark:border-white dark:hover:bg-gray-700 dark:hover:text-white">
+          <span class="sr-only">Next</span>
+          <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path fill="#FFF" fill-rule="evenodd" class="fill-white dark:fill-white"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"></path>
+          </svg>
+        </a>
+      </li>
+      <li @click="lastPage">
+        <a href="#"
+        class="flex flex-row  px-2 py-2 ml-0 leading-tight text-white bg-gray-600 border border-gray-300  hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white rounded-r-lg">
+          <i class="fa-solid fa-greater-than text-sm"></i>
+          <i class="fa-solid fa-greater-than text-sm"></i>
+        </a>
+      </li>
+    </ul>
+    <small class="ml-4 text-sm font-medium">Page {{ currentPageHuman }} of {{ numPages }}</small>
+  </nav>
+  <!-- <div :class="lightBorderStyle" class="p-3 lg:px-6 border-t dark:border-gray-800">
     <Level>
       <jb-buttons>
         <jb-button v-for="item in pagesList" :key="item" :active="item === currentPage" :label="item + 1"
@@ -198,5 +296,12 @@ const pageNext = (page) => {
       </jb-buttons>
       <small>Page {{ currentPageHuman }} of {{ numPages }}</small>
     </Level>
-  </div>
+  </div> -->
 </template>
+<style>
+
+li.active,li.active:hover {
+  background-color: rgb(48, 48, 48);
+  color: white;
+}
+</style>
