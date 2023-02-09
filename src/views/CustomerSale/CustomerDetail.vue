bde6bd0b8b96317bae5cec9cc82bc8042b0d0451
BoatPawarisorn <boatzeed57270@gmail.com>
2023-02-09 17:58:10 +0700

    update layout detail

--- src/views/CustomerSale/CustomerDetail.vue
+++ src/views/CustomerSale/CustomerDetail.vue
@@ -45,7 +45,7 @@ onMounted(() => {
     states.note = res.data.data.note;
     states.contactBy = res.data.data.contactBy;
     states.status = res.data.data.status;
-    states.otherCustomerProduct = res.data.data.otherCustomerProduct ;  //สินค้า
+    states.otherCustomerProduct = res.data.data.otherCustomerProduct;  //สินค้า
     states.otherCustomerLog = res.data.data.otherCustomerLog; // log
 
 
@@ -65,13 +65,13 @@ const reversedArray = computed(() => {
 });
 
 
-const formatdate =(date)=>{
+const formatdate = (date) => {
   let str = date.toString();
   // console.log(d);
-  let x = str.substring(str.indexOf('T')+1);
-  let time = x.substring(0,x.indexOf('.000Z'));
+  let x = str.substring(str.indexOf('T') + 1);
+  let time = x.substring(0, x.indexOf('.000Z'));
 
-  return  time.slice(0, 5);
+  return time.slice(0, 5);
 };
 
 
@@ -88,56 +88,57 @@ const formatdate =(date)=>{
         </div>
         <div class="flex justify-center">
           <div class="flex flex-col w-custom p-4">
-          <div class="font-bold mb-2">ชื่อ :
-            <span v-if="states.name == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <span v-else class="font-normal text-gray-800">{{ states.name }}</span>
+            <div class="font-bold mb-2">ชื่อ :
+              <span v-if="states.name == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <span v-else class="font-normal text-gray-800">{{ states.name }}</span>
+            </div>
+            <div class="font-bold mb-2">เบอร์โทร :
+              <span v-if="states.phone == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <span v-else class="font-normal text-gray-800">{{ states.phone }}</span>
+            </div>
+            <div class="font-bold mb-2">ที่อยู่ :
+              <span v-if="states.address == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <!-- <span v-else class="font-normal text-gray-800">{{ states.address }} ต.{{ states.subdistrict_name }} อ.{{ states.district_name }} จ. {{ states.province_name }} {{ states.postcode }}</span> -->
+              <span v-else class="font-normal text-gray-800">{{ states.address }}</span>
+            </div>
+            <div class="font-bold mb-2">จังหวัด :
+              <span v-if="states.province_name == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <span v-else class="font-normal text-gray-800">{{ states.province_name }}</span>
+            </div>
+            <div class="font-bold mb-2">อำเภอ :
+              <span v-if="states.district_name == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <span v-else class="font-normal text-gray-800">{{ states.district_name }}</span>
+            </div>
+            <div class="font-bold mb-2">ตำบล :
+              <span v-if="states.subdistrict_name == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <span v-else class="font-normal text-gray-800">{{ states.subdistrict_name }}</span>
+            </div>
+            <div class="font-bold mb-2">รหัสไปรษณีย์ :
+              <span v-if="states.postcode == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <span v-else class="font-normal text-gray-800">{{ states.postcode }}</span>
+            </div>
+            <div class="font-bold mb-2">ประเภท :
+              <span v-if="states.type == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <span v-else class="font-normal text-gray-800">{{ states.type }}</span>
+            </div>
+            <div class="font-bold mb-2 ">รายละเอียด :
+              <p v-if="states.detail == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</p>
+              <p v-else class="font-normal text-gray-800 indent-10" style="word-wrap: break-word;">{{ states.detail }}
+              </p>
+            </div>
+            <div class="font-bold mb-2">ช่องทาง :
+              <span v-if="states.contactBy == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <span v-else class="font-normal ">{{ states.contactBy }}</span>
+            </div>
+            <div class="font-bold mb-2">สถานะ :
+              <span v-if="states.status == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <span v-else class="font-normal">{{ states.status }}</span>
+            </div>
+            <div class="font-bold mb-2">หมายเหตุ :
+              <span v-if="states.note == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
+              <span v-else class="font-normal">{{ states.note }}</span>
+            </div>
           </div>
-          <div class="font-bold mb-2">เบอร์โทร :
-            <span v-if="states.phone == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <span v-else class="font-normal text-gray-800">{{ states.phone }}</span>
-          </div>
-          <div class="font-bold mb-2">ที่อยู่ :
-            <span v-if="states.address == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <!-- <span v-else class="font-normal text-gray-800">{{ states.address }} ต.{{ states.subdistrict_name }} อ.{{ states.district_name }} จ. {{ states.province_name }} {{ states.postcode }}</span> -->
-            <span v-else class="font-normal text-gray-800">{{ states.address }}</span>
-          </div>
-          <div class="font-bold mb-2">จังหวัด :
-            <span v-if="states.province_name == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <span v-else class="font-normal text-gray-800">{{ states.province_name }}</span>
-          </div>
-          <div class="font-bold mb-2">อำเภอ :
-            <span v-if="states.district_name == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <span v-else class="font-normal text-gray-800">{{ states.district_name }}</span>
-          </div>
-          <div class="font-bold mb-2">ตำบล :
-            <span v-if="states.subdistrict_name == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <span v-else class="font-normal text-gray-800">{{ states.subdistrict_name }}</span>
-          </div>
-          <div class="font-bold mb-2">รหัสไปรษณีย์ :
-            <span v-if="states.postcode == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <span v-else class="font-normal text-gray-800">{{ states.postcode }}</span>
-          </div>
-          <div class="font-bold mb-2">ประเภท :
-            <span v-if="states.type == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <span v-else class="font-normal text-gray-800">{{ states.type }}</span>
-          </div>
-          <div class="font-bold mb-2 ">รายละเอียด :
-            <p v-if="states.detail == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</p>
-            <p v-else class="font-normal text-gray-800 indent-10" style="word-wrap: break-word;">{{ states.detail }}</p>
-          </div>
-          <div class="font-bold mb-2">ช่องทาง :
-            <span v-if="states.contactBy == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <span v-else class="font-normal ">{{ states.contactBy }}</span>
-          </div>
-          <div class="font-bold mb-2">สถานะ :
-            <span v-if="states.status == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <span v-else class="font-normal">{{ states.status }}</span>
-          </div>
-          <div class="font-bold mb-2">หมายเหตุ :
-            <span v-if="states.note == ''" class="opacity-60 font-normal">ไม่ได้ระบุ</span>
-            <span v-else class="font-normal">{{ states.note }}</span>
-          </div>
-        </div>
         </div>
 
       </div>
@@ -146,7 +147,7 @@ const formatdate =(date)=>{
       <h6 class="text-center font-bold text-lg mb-4">สถานะดำเนินการ</h6>
       <div v-if="(states.otherCustomerLog).length !=0">
         <div class="relative overflow-x-auto">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <!-- <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-800 uppercase  dark:text-gray-400 ">
               <tr class="bg-gray-100">
                 <th scope="col" class="w-[10px] p-0"></th>
@@ -200,126 +201,148 @@ const formatdate =(date)=>{
                 </td>
               </tr>
             </tbody>
-          </table>
+          </table> -->
+          <div class="mx-10">
+            <ol class="relative border-l border-gray-200 dark:border-gray-700">
+              <li class="mb-10 ml-6 bg-white  w-[500px] shadow-custom-test p-2 rounded-lg"  v-for="(log, index) in reversedArray" :key="index">
+                <span :class="index==0?'bg-red-100':'bg-blue-100'"
+                  class="absolute flex items-center justify-center w-6 h-6  rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <i v-if="index==0" class="fa-solid fa-fire text-red-500"></i>
+                  <i v-else class="fa-solid fa-circle-dot"></i>
+                </span>
+                <h3 class="flex items-center mb-1 text-base font-semibold text-gray-900 dark:text-white">{{ log.status }}
+                  <span v-if="index==0" class="bg-red-200 text-black text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-blue-300 ml-3">ล่าสุด</span>
+                </h3>
+                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{ moment(new Date(log.createdAt)).format('DD-MM-YYYY') }}
+                  เวลา {{ formatdate(log.createdAt) }} น.</time>
+                <p class="mb-4 text-base font-normal text-gray-900 "><span class="text-black font-medium">รายละเอียด :</span> {{ log.detailLog }}</p>
+              </li>
+            </ol>
+          </div>
         </div>
 
       </div>
       <div v-else class="text-center ">
-        <span class="opacity-70 " >ยังไม่มีข้อมูล</span>
+        <span class="opacity-70 ">ยังไม่มีข้อมูล</span>
       </div>
     </div>
   </div>
   <div>
     <div class=" bg-white  shadow-custom h-[500px] m-4 rounded-lg py-10 px-0 overflow-y-auto">
-        <h1 class="text-center text-lg font-semibold mb-4">สินค้าที่สั่งซื้อทั้งหมด</h1>
-        <div v-if="(states.otherCustomerProduct).length !=0" class="relative overflow-x-auto mx-1 ">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
-            <thead class="text-base text-gray-900  bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" class="px-4 py-3 truncate ">
-                  ชื่อสินค้า
-                </th>
-                <th scope="col" class="px-4 py-3 truncate">
-                  Serial Number
-                </th>
-                <th scope="col" class="px-4 py-3 truncate ">
-                  วันที่ซื้อ
-                </th>
-                <th scope="col" class="px-4 py-3 truncate">
-                  วันที่หมดประกัน
-                </th>
-                <th scope="col" class="px-4 py-3 truncate ">
-                  ใบประเมินราคา
-                </th>
-                <th scope="col" class="px-4 py-3 truncate">
-                  ใบเสนอราคา
-                </th>
+      <h1 class="text-center text-lg font-semibold mb-4">สินค้าที่สั่งซื้อทั้งหมด</h1>
+      <div v-if="(states.otherCustomerProduct).length !=0" class="relative overflow-x-auto mx-1 ">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+          <thead class="text-base text-gray-900  bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-4 py-3 truncate ">
+                ชื่อสินค้า
+              </th>
+              <th scope="col" class="px-4 py-3 truncate">
+                Serial Number
+              </th>
+              <th scope="col" class="px-4 py-3 truncate ">
+                วันที่ซื้อ
+              </th>
+              <th scope="col" class="px-4 py-3 truncate">
+                วันที่หมดประกัน
+              </th>
+              <th scope="col" class="px-4 py-3 truncate ">
+                ใบประเมินราคา
+              </th>
+              <th scope="col" class="px-4 py-3 truncate">
+                ใบเสนอราคา
+              </th>
 
-                <th scope="col" class="px-6 py-3 truncate ">
-                  บริการเสริม
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  อื่นๆ
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="bg-white dark:bg-gray-800 " v-for="(product,index) in states.otherCustomerProduct" :key="index">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white bg-gray-50">
-                  {{ product.name }}
-                </th>
-                <td class="px-6 py-4">
-                  {{ product.serialNumber }}
-                </td>
-                <td class="px-6 py-4 bg-gray-50">
-                  <span v-if=" product.purchaseDate == null">-</span>
-                  <span v-else>{{ moment(new Date( product.purchaseDate)).format('DD/MM/YYYY') }}</span>
+              <th scope="col" class="px-6 py-3 truncate ">
+                บริการเสริม
+              </th>
+              <th scope="col" class="px-6 py-3">
+                อื่นๆ
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-white dark:bg-gray-800 " v-for="(product,index) in states.otherCustomerProduct" :key="index">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white bg-gray-50">
+                {{ product.name }}
+              </th>
+              <td class="px-6 py-4">
+                {{ product.serialNumber }}
+              </td>
+              <td class="px-6 py-4 bg-gray-50">
+                <span v-if=" product.purchaseDate == null">-</span>
+                <span v-else>{{ moment(new Date( product.purchaseDate)).format('DD/MM/YYYY') }}</span>
 
-                </td>
-                <td class="px-6 py-4">
-                  <span v-if=" product.warrantyExpired == null">-</span>
-                  <span v-else>{{ moment(new Date( product.warrantyExpired)).format('DD/MM/YYYY') }}</span>
+              </td>
+              <td class="px-6 py-4">
+                <span v-if=" product.warrantyExpired == null">-</span>
+                <span v-else>{{ moment(new Date( product.warrantyExpired)).format('DD/MM/YYYY') }}</span>
 
-                </td>
-                <td class="px-6 py-4 bg-gray-50 text-center">
-                  <div v-if="product.estimate == 'string' || product.estimate ==''">
-                    -
-                  </div>
-                  <div v-else>
-                    <a :href="product.estimate" target="_blank" class="hover:text-blue-500 flex flex-col"><i class="fa-solid fa-file text-lg"></i><span>link</span></a>
-                  </div>
-                </td>
-                <td class="px-6 py-4 text-center">
-                  <div v-if="product.quotation == 'string' || product.quotation == ''">
-                    -
-                  </div>
-                  <div v-else>
-                    <a :href="product.quotation" target="_blank" class="hover:text-blue-500 flex flex-col"><i class="fa-solid fa-file text-lg"></i><span>link</span></a>
-                  </div>
-                </td>
-                <td class="px-6 py-4 bg-gray-50">
-                  <div v-if="product.additionalServices == 'string' || product.additionalServices == ''">
-                    -
-                  </div>
-                  <div v-else>
-                    {{ product.additionalServices }}
-                  </div>
-                </td>
-                <td class="px-6 py-4">
-                  <div v-if="product.etc == 'string' || product.etc == ''">
-                    -
-                  </div>
-                  <div v-else>
-                    {{ product.etc }}
-                  </div>
+              </td>
+              <td class="px-6 py-4 bg-gray-50 text-center">
+                <div v-if="product.estimate == 'string' || product.estimate ==''">
+                  -
+                </div>
+                <div v-else>
+                  <a :href="product.estimate" target="_blank" class="hover:text-blue-500 flex flex-col"><i
+                      class="fa-solid fa-file text-lg"></i><span>link</span></a>
+                </div>
+              </td>
+              <td class="px-6 py-4 text-center">
+                <div v-if="product.quotation == 'string' || product.quotation == ''">
+                  -
+                </div>
+                <div v-else>
+                  <a :href="product.quotation" target="_blank" class="hover:text-blue-500 flex flex-col"><i
+                      class="fa-solid fa-file text-lg"></i><span>link</span></a>
+                </div>
+              </td>
+              <td class="px-6 py-4 bg-gray-50">
+                <div v-if="product.additionalServices == 'string' || product.additionalServices == ''">
+                  -
+                </div>
+                <div v-else>
+                  {{ product.additionalServices }}
+                </div>
+              </td>
+              <td class="px-6 py-4">
+                <div v-if="product.etc == 'string' || product.etc == ''">
+                  -
+                </div>
+                <div v-else>
+                  {{ product.etc }}
+                </div>
 
-                </td>
-              </tr>
-            </tbody>
+              </td>
+            </tr>
+          </tbody>
 
-          </table>
-        </div>
-        <div v-else class="text-center mt-10">
-          <span class="opacity-70">ยังไม่มีข้อมูลสินค้า</span>
-        </div>
+        </table>
       </div>
+      <div v-else class="text-center mt-10">
+        <span class="opacity-70">ยังไม่มีข้อมูลสินค้า</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
 .shadow-custom {
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+.shadow-custom-test {
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgb(209, 213, 219) 0px 0px 0px 1px inset;
 }
 
 
 @media (max-width:1160) {
-  .w-custom  {
+  .w-custom {
     width: 550px;
   }
 }
+
 @media (min-width:1160) {
-  .w-custom  {
+  .w-custom {
     width: 700px;
   }
 }
