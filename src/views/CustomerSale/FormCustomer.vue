35e6c12b18cc12ae978de3308de4882d25d6a24b
BoatPawarisorn <boatzeed57270@gmail.com>
2023-02-23 15:06:43 +0700

    update ui upload image

--- src/views/CustomerSale/FormCustomer.vue
+++ src/views/CustomerSale/FormCustomer.vue
@@ -840,6 +840,8 @@ const deleteItem = (index, customer, product_id) => {
 
 let spiner_load = ref(false);
 
+
+let len_over = ref(false);
 // ============ function  Upload Image =======================
 const upload_image = async (event)=>{
   spiner_load.value = true ;
@@ -866,6 +868,7 @@ const upload_image = async (event)=>{
             if(dataform.otherCustomerImage.length < 5){
               dataform.otherCustomerImage.push({imageName:res.data});
             } else{
+              len_over.value = true ;
               Swal.fire({
                 icon: "error",
                 title: "อัปโหลดรูปภาพได้ไม่เกิน 5 รูป",
@@ -878,6 +881,8 @@ const upload_image = async (event)=>{
               dataform.otherCustomerImage.push(res.data);
             }
             else{
+
+              len_over.value = true ;
               Swal.fire({
                 icon: "error",
                 title: "อัปโหลดรูปภาพได้ไม่เกิน 5 รูป",
@@ -911,6 +916,7 @@ const upload_image = async (event)=>{
   }
   // ถ้ารูปเกิน
   else{
+    len_over.value = true ;
     Swal.fire({
       icon: "warning",
       title:'จำนวนรูปภาพเยอะเกินไป !',
@@ -918,9 +924,14 @@ const upload_image = async (event)=>{
       timer:4000
     });
 
-  document.getElementById('file_input').value = "";
   const input_file = document.getElementById('file_input') ;
-  input_file.classList.add('border-[#F05252]');
+
+  if(len_over.value == true){
+    input_file.classList.add('bg-[#FDF2F2]');
+  }
+  else {
+    input_file.classList.add('bg-[#F3FAF7]');
+  }
   spiner_load.value = false ;
 
   }
@@ -1175,33 +1186,40 @@ else {
                   </div>
                 </div>
                 <!-- Upload File image -->
-                <div class="mb-2">
-                  <label class="block mb-2 text-base font-medium text-gray-900 dark:text-white" for="file_input">อัปโหลดรูปภาพ</label>
-                  <div class="flex flex-row items-center">
-                    <input @change="upload_image" accept=".jpeg,.png ,.jpg"  id="file_input" type="file" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-300 focus:border-gray-300 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-300 dark:focus:border-gray-300"  multiple>
-                    <div v-if="spiner_load == true" class="ml-2">
+                <div>
+                  <label for="uoload" class="block mb-2 text-base font-medium text-black dark:text-white" >อัปโหลดรูปภาพ</label>
+                  <label for="dropzone-file" id="file_input" class="flex flex-col items-center justify-center w-full h-[150px] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                      <div v-if="spiner_load == true" class="ml-2">
                       <svg aria-hidden="true" class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                    </svg>
+                      </svg>
+                      <span class="text-md font-bold">กำลังอัปโหลด</span>
                     </div>
-                  </div>
-                  <p class="mt-1 text-xs text-gray-500 dark:text-gray-300" id="file_input_help">PNG, JPG or JPEG</p>
-                </div>
-                <div v-if="id" class="flex flex-row items-center gap-4">
+                    <div v-else class="flex flex-col items-center justify-center pt-5 pb-6">
+                          <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                          <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">คลิกเพื่ออัปโหลดรูปภาพ </span></p>
+                          <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG , JPEG </p>
+                      </div>
+                      <input id="dropzone-file"  @change="upload_image" type="file" accept=".png ,.jpg ,.jpeg,.svg" class="hidden"  multiple/>
+                  </label>
+                  <div v-if="id" class="flex flex-row items-center gap-2 mt-4">
                   <div v-for="(item,index) in dataform.otherCustomerImage" :key="index" class="text-center relative">
                     <button type="button" @click="delete_img(item.imageName,'other',index)" class="bg-red-500 hover:bg-red-400 text-white  py-0 px-2 rounded-lg absolute -right-2 -top-3 "><i class="fa fa-close"></i></button>
                     <img :src="`/api-main/image/${item.imageName}?imageableType=other`"  alt="" class="object-cover w-[150px] h-[150px] rounded-lg">
                     <span class="block w-[150px] truncate">{{ item.imageName }}</span>
                   </div>
                 </div>
-                <div v-else class="flex flex-row items-center gap-4">
+                <div v-else class="flex flex-row items-center gap-2  mt-4">
                   <div v-for="(item,index) in dataform.otherCustomerImage" :key="index" class="text-center relative">
                     <button type="button" @click="delete_img(item,'other',index)" class="bg-red-500 hover:bg-red-400 text-white  py-0 px-2 rounded-lg absolute -right-2 -top-3 "><i class="fa fa-close"></i></button>
                     <img :src="`/api-main/image/${item}?imageableType=other`"  alt="" class="object-cover w-[150px] h-[150px] rounded-lg">
                     <span class="block w-[150px] truncate">{{ item }}</span>
                   </div>
                 </div>
+                </div>
+
+
                 <div class="mb-6 w-full mt-6">
                   <label for="note" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">หมายเหตุ
                     <span class="text-black opacity-50">(ไม่บังคับ)</span></label>
